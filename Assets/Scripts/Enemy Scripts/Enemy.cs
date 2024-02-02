4187ef568f8f1450fe85d791eddb12a20b463d48
Shanaya Carvalho <shanayarose31@gmail.com>
2024-02-02 19:28:53 +0530

    done 2/2/2024

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -2,87 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
+
     private Transform playerTarget;
 
-    [SerializeField]
-    private float moveSpeed = 2f;
+    [SerializeField] private float moveSpeed = 2f;
 
     private Vector3 tempScale;
 
-    [SerializeField]
-    private float stoppingDistance = 1.5f;
+    [SerializeField] private float stoppingDistance;
 
     private PlayerAnimation enemyAnimation;
 
-    [SerializeField]
-    private float attackWaitTime = 2.5f;
+    [SerializeField] private float attackWaitTime = 2.5f;
     private float attackTimer;
 
-    [SerializeField]
-    private float attackFinishedWaitTime = 0.5f;
+    [SerializeField] private float attackFinishedWaitTime = 0.5f;
     private float attackFinishedTimer;
 
-    private void Awake()
-    {
+    [SerializeField] private EnemyDamageArea enemyDamageArea;
+
+
+    private bool enemyDied;
+
+    [SerializeField] private float deathAniTime;
+
+    [SerializeField] RectTransform healthBarTransform;
+    private Vector3 healthBarTempScale;
+
+    private void Awake() {
         playerTarget = GameObject.FindWithTag(TagManager.PLAYER_TAG).transform;
-        enemyAnimation = GetComponent<PlayerAnimation>();
+
+        enemyAnimation = GetComponent<PlayerAnimation>();   
     }
 
-    private void Update()
-    {
+    private void Update() {
+
+        if (enemyDied) {
+            return;
+        }
         SearchForPlayer();
     }
 
-    void SearchForPlayer()
-    {
-        if (!playerTarget)
+    void SearchForPlayer() {
+        if (!playerTarget) {
             return;
+        }
 
-        if (Vector3.Distance(transform.position, playerTarget.position) > stoppingDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, playerTarget.position, moveSpeed = Time.deltaTime);
+        if (Vector3.Distance(transform.position, playerTarget.position) > stoppingDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, playerTarget.position, moveSpeed * Time.deltaTime);
 
             enemyAnimation.PlayAnimation(TagManager.WALK_ANIMATION_NAME);
+
             HandleFacingDirection();
         }
-        else
-        {
+        else {
             CheckIfAttackFinished();
             Attack();
-
         }
-
-
+      
     }
 
-    void HandleFacingDirection()
-    {
+    void HandleFacingDirection() {
+
         tempScale = transform.localScale;
 
-        if (transform.position.x > playerTarget.position.x)
+        if (transform.position.x > playerTarget.position.x) {
             tempScale.x = Mathf.Abs(tempScale.x);
-        else
+        }
+        else {
             tempScale.x = -Mathf.Abs(tempScale.x);
+        }
 
         transform.localScale = tempScale;
+
+        //Health Bar Direction Facing
+        healthBarTempScale = healthBarTransform.localScale;
+
+        if(transform.localScale.x > 0f) {
+            healthBarTempScale.x = Mathf.Abs(healthBarTempScale.x);
+        }
+        else {
+            healthBarTempScale.x = -Mathf.Abs(healthBarTempScale.x);
+        }
+
+        healthBarTransform.localScale = healthBarTempScale;
     }
 
-    void CheckIfAttackFinished()
-    {
-        if (Time.time > attackFinishedTimer)
+    void CheckIfAttackFinished() {
+        if(Time.time > attackFinishedTimer) {
             enemyAnimation.PlayAnimation(TagManager.IDLE_ANIMATION_NAME);
+        }
     }
 
-    void Attack()
-    {
-        if(Time.time > attackTimer)
-        {
+    void Attack() {
+
+        if(Time.time > attackTimer) {
+
             attackFinishedTimer = Time.time + attackFinishedWaitTime;
             attackTimer = Time.time + attackWaitTime;
 
             enemyAnimation.PlayAnimation(TagManager.ATTACK_ANIMATION_NAME);
         }
     }
+
+    void EnemyAttack() {
+        enemyDamageArea.gameObject.SetActive(true);
+        enemyDamageArea.ResetDeactivateTimer();
+    }
+
+    public void EnemyDied() {
+        enemyDied = true;
+        enemyAnimation.PlayAnimation(TagManager.DEATH_ANIMATION_NAME);
+        Invoke("DestroyEnemyAfterDelay",deathAniTime);
+    }
+
+    void DestroyEnemyAfterDelay() {
+        Destroy(gameObject);
+    }
+
 }
